9804ffbea02602abbe00280bc28329cf23292b87
bhavin1998 <bhavingediya123@gmail.com>
2022-06-22 16:32:18 +0530

    first commit

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '202564412028-ocpahdg5te7srjgm0b7hulf8g4dadpkm.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', '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');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/google-calender-event/google-login.php');
+
+?>
